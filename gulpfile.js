946114ead9be3c3e5d23f75a428a9819e02277c3
Adam Lohner <adamlohner@pendo.io>
2018-08-15 22:42:33 -0400

    update element-font files and remove feather icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var cssmin = require("gulp-cssmin");
+var clean = require("gulp-rimraf");
 
-gulp.task('compile', function() {
-    return gulp
-        .src('./src/*.scss')
-        .pipe(sass.sync())
-        .pipe(
-            autoprefixer({
-                browsers: ['ie >= 11', 'last 2 versions'],
-                cascade: false
-            })
-        )
-        .pipe(cssmin())
-        .pipe(gulp.dest('./lib'));
+gulp.task("clean", [], function() {
+  return gulp.src("lib/*", { read: false }).pipe(clean());
 });
 
-gulp.task('fonts', function() {
-    return gulp
-        .src('./src/fonts/**')
-        .pipe(cssmin())
-        .pipe(gulp.dest('./lib/fonts'));
+gulp.task("compile", ["clean"], function() {
+  return gulp
+    .src("./src/*.scss")
+    .pipe(sass.sync())
+    .pipe(
+      autoprefixer({
+        browsers: ["ie >= 11", "last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(cssmin())
+    .pipe(gulp.dest("./lib"));
 });
 
-gulp.task('icons', function() {
-    return gulp.src('./src/feather-icons/**').pipe(gulp.dest('./lib/feather-icons'));
+gulp.task("fonts", function() {
+  return gulp.src("./src/fonts/**").pipe(gulp.dest("./lib/fonts"));
 });
 
-gulp.task('build', ['compile', 'fonts', 'icons']);
+gulp.task("icons", function() {
+  return gulp
+    .src("./src/feather-icons/**")
+    .pipe(gulp.dest("./lib/feather-icons"));
+});
+
+gulp.task("build", ["compile", "fonts"]);
